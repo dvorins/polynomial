f65698fd6ebee2d2e2d18e32bf66a4a9fce2afc2
Sam Dvorin <samdvorin@Sams-MacBook-Pro-2.local>
2024-01-28 20:24:18 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x_value):
+        return x_value
+
 class Int:
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, x_value):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -22,6 +28,10 @@ class Add:
         repr_p2 = f"( {repr(self.p2)} )" if isinstance(self.p2, (Add, Sub, Div, Mul)) else repr(self.p2)
         
         return repr_p1 + " + " + repr_p2
+    
+    def evaluate(self, x_value):
+            return self.p1.evaluate(x_value) + self.p2.evaluate(x_value)
+
 
 class Mul:
     def __init__(self, p1, p2):
@@ -34,6 +44,10 @@ class Mul:
         
         return repr_p1 + " * " + repr_p2
 
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) * self.p2.evaluate(x_value)
+
+
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -44,6 +58,10 @@ class Div:
         repr_p2 = f"( {repr(self.p2)} )" if isinstance(self.p2, (Add, Sub, Div, Mul)) else repr(self.p2)
         
         return repr_p1 + " / " + repr_p2
+
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) / self.p2.evaluate(x_value)
+
 
 class Sub:
     def __init__(self, p1, p2):
@@ -56,13 +74,21 @@ class Sub:
         
         return repr_p1 + " - " + repr_p2
 
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) - self.p2.evaluate(x_value)
+
+
 
 
 poly1 = Add(Int(2), Mul(X(), Int(3))) # should be 2 + (X * 3)
-print(poly1, "\n")
+print(poly1)
+result = poly1.evaluate(5)
+print(result, "\n")
  
 poly2 = Sub(Mul(X(), Int(5)), Div(Int(10), X()))
-print(poly2, "\n") 
+print(poly2)
+result = poly2.evaluate(5)
+print(result, "\n") 
 
 poly3 = Add(Add(Int(2), Mul(X(), Int(3))), Sub(Mul(X(), X()), Div(X(), Int(2))))
 print(poly3, "\n") 
